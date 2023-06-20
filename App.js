61bf5add446d0e8280a292878edd616bd3340cdd
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-06-20 15:04:00 +0300

    INITIAL APP

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
 import { StatusBar } from "expo-status-bar";
-
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { useRoute } from "./router";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import Main from "./components/Main";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App({ navigation }) {
-  const route = useRoute(true);
-  return <NavigationContainer>{route}</NavigationContainer>;
+  const [isReady, setIsReady] = useState(false);
+
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
+          RobotoMedium: require("./assets/fonts/Roboto-Medium.ttf"),
+          RobotoBold: require("./assets/fonts/Roboto-Bold.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
+  return (
+    <Provider store={store}>
+      <Main onLayout={onLayoutRootView} />
+    </Provider>
+  );
 }
